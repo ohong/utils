b5a3e5d6db100fc65be1b97aa9f8b015c61b8509
Oscar Hong <oscar.hong2015@gmail.com>
2024-09-12 00:24:32 -0700

    new util for estimating reading speed of a new post

---
+++ reading-time.js
@@ -0,0 +1,13 @@
+if (document.querySelector("body").classList.contains("post")) {
+    const readingTime = Math.ceil(document.querySelector("main").innerText.trim().split(/\s+/).length / parseInt(document.currentScript.getAttribute("data-wpm") ?? 255));
+    document
+        .querySelector("main")
+        .insertBefore(
+            document.body.appendChild(
+                Object.assign(
+                    document.createElement("p"), {
+                    className: "reading-time",
+                    innerHTML: `Reading time: ${readingTime} minute${readingTime > 1 ? "s" : ""}`
+                })),
+            document.querySelector("main").childNodes[parseInt(document.currentScript.getAttribute("data-before-child") ?? 4)]);
+}
